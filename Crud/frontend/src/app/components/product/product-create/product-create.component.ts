71f917b0984cbfad35124902e8d23c45f7e5ba7a
GustavoRSL <d7gustavo@yahoo.com.br>
2021-12-18 08:54:12 -0300

    Criando bindings de atributo e evento.

--- Crud/frontend/src/app/components/product/product-create/product-create.component.ts
+++ Crud/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
+  atributoQualquer = "Qualquer"
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  fazerAlgo(): void {
+    console.log('Fazendo algo!')
   }
 
 }
